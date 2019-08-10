01ee7838f5e91db44680ce89994b3c03f3858534
Tobias Locsei <jtlocsei@gmail.com>
2019-08-10 22:49:30 +0100

    Update description

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject latex-helper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject latex-helper "0.1.0"
+  :description "A tiny library with some helpers for writing LaTeX"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns latex-helper.core})
+  :repl-options {:init-ns latex-helper.core}
+  :plugins [[lein-codox "0.10.7"]])
