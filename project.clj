27adfc4a0e826726db21c762ab00eff414fd3c20
Tobias Locsei <jtlocsei@gmail.com>
2019-08-11 17:17:06 +0100

    Update project url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject latex-helper "0.1.0"
   :description "A tiny clojure library with some helpers for writing LaTeX, e.g. escaping special characters, and turning dumb quotes to smart quotes."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jtlocsei/latex-helper"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
